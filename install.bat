ca179d0c499553d03b14b54c8c442fda9f2f6312
delanobgt <irvindelano@gmail.com>
2018-08-20 13:16:19 +0700

    added batch script

---
+++ install.bat
@@ -0,0 +1,2 @@
+setx LAB_PC_NUMBER "5" /m
+setx LAB_LOCATION "AD107" /m
---
+++ start_java_login_kiosk.bat
@@ -0,0 +1,2 @@
+:: C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
+start javaw -jar D:\Java\java-UPH-computer-lab-client\dist\java-UPH-computer-lab-client.jar
